62264dadb3cab47e6977d47db63563d1d76ecac3
bob walker <bob@randomness.org.uk>
2013-01-05 18:19:30 +0000

    make so that this restartable.

--- flickruploaddir.sh
+++ flickruploaddir.sh
@@ -2,9 +2,11 @@
 #script to upload all files in the current dir
 count=0
 total=`ls |wc -l`
+mkdir -p uploaded
 for file in * 
 do
 (( count=count+1 ))
 echo "uploading $count of $total"
 flickrupload.sh $file
+mv $file uploaded/$file
 done
